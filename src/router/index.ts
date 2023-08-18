a3dfe2faf647f740c46e0602779a13d2df9ae4de
Ugur Diktas <diktas.ugur@gmail.com>
2023-08-18 21:54:01 +0200

    Built placeholder website until page is productive

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
+import LandingPage from '../views/LandingPage.vue'
 import Test from '../views/Test.vue'
 
 
 const routes: Array<RouteRecordRaw> = [
+
+    {
+        path: '/',
+        name: 'LandingPage',
+        component: LandingPage
+    },
 
     {
         path: '/test',
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello :)</h1>
+        <p>Diese Webseite wird erst noch gebaut!</p>
+        <p>Bitte schau später nochmal vorbei. Stand: August 2023</p>
+        <p>(╯°□°）╯︵ ┻━┻</p>
+        <p>^^ me rn ^^</p>
+        <RouterLink to="test">
+            <button>
+                Wenn dir ganz langweilig ist, kannst du hier auf eine versteckte Seite navigieren ;)
+            </button>
+        </RouterLink>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
